341c3b8a47f2b7fd781681fa4c808486dbd6756b
Tom Driscoll <tdriscoll1@yahoo.com>
2020-03-28 12:34:35 -0400

    nav wrap

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org"fail
           target="_blank"
           rel="noopener noreferrer"
         >
