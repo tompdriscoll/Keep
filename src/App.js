1f91fbbd94e2f43c756372ef227c81a835950f35
Tom Driscoll <tdriscoll1@yahoo.com>
2020-03-28 17:50:34 -0400

    basic menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import {Nav} from './nav/Nav.jsx'
+import {Menu} from './menu/menu.jsx'
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Nav/>
+        <Menu/>
       </header>
     </div>
   );
--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -6,19 +6,19 @@ export function Nav(){
         <div id='nav-wrapper'>
             <div id='nav-contents' className='nav-section'>
                 <div id='nav-left'>
-                <div classNmae='svg-holder'><svg class='nav-svg'><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg></div>
+                <div id='hamburger-holder' className='svg-holder'><svg><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg></div>
                 <img class="nav-icon" src="https://www.gstatic.com/images/branding/product/1x/keep_48dp.png" />
                 <span id='keep-text'>Keep</span>
                 </div>
                 <div id='nav-right' className='nav-section'>
                 <div id='nav-search' className='nav-section'>
                     <div id='search-box'>
-                        <svg class='nav-svg'><path d="M20.49,19l-5.73-5.73C15.53,12.2,16,10.91,16,
+                    <div className='svg-holder'><svg class='nav-svg'><path d="M20.49,19l-5.73-5.73C15.53,12.2,16,10.91,16,
                         9.5C16,5.91,13.09,3,9.5,3S3,5.91,3,9.5C3,13.09,5.91,16,9.5,
                         16 c1.41,0,2.7-0.47,3.77-1.24L19,20.49L20.49,19z M5,9.5C5,
                         7.01,7.01,5,9.5,5S14,7.01,14,9.5S11.99,14,9.5,14S5,11.99,5,9.5z"></path>
-                        </svg>
-                        <span id='search-text'>Search</span>
+                        </svg></div>
+                        <span id='search-text' type='text'>Search</span>
                     </div>
                 </div>
                 <div id='nav-right-icons'>
@@ -58,6 +58,27 @@ export function Nav(){
                     </div>
                     
                 </div>
+                </div>
+                <div id='nav-end'>
+                    <div className='svg-holder'>
+                        <svg className='nav-svg'>
+                        <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,
+                        2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,
+                        2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,
+                        2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,
+                        2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,
+                        2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,
+                        0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,
+                        2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,
+                        2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                        </svg>
+
+                    </div>
+                    <div className='svg-holder'>
+                    <svg id='profile-pic'>
+                        <circle xmlns="http://www.w3.org/2000/svg" cx="14" cy="14" r="14"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
         </div>
