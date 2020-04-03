b1c7b4bf1a89127daa0aac9dbaa0189d52cc9cd5
Tom Driscoll <tdriscoll1@yahoo.com>
2020-04-02 23:56:08 -0400

    msp mdp

---
+++ src/main/form_container.js
@@ -0,0 +1,20 @@
+import connect from 'react-redux'
+import Form from './form'
+
+import { receiveTodos, receiveTodo } from '../actions/todo_actions';
+import { allTodos } from '../reducers/selectors';
+
+const msp = state => ({
+    todos: allTodos(state),
+    state
+  });
+  
+  const mdp = dispatch => ({
+    receiveTodo: todo => dispatch(receiveTodo(todo))
+  });
+  
+  export default connect(
+    msp,
+    mdp
+  )(TodoList);
+  
